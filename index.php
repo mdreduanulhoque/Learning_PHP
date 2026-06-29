073dce2082e771a20fbc927682e5a60bb05d4bb0
MD Reduanul Hoque <mdreduanulhoquesadik@gmail.com>
2026-06-29 08:47:54 +0600

    index.php echo statement

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<?php
+    echo "Hello PHP!! <br>";
+    echo 'Hello PHP!! <br>';
+    echo "Hello " . "PHP" . "!! <br> <br>";
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello PHP!! <br>";
+    echo 'Hello PHP!! <br>';
+    echo "Hello " . "PHP" . "!! <br> <br>";
+    ?>
+    
+</body>
+</html>
+
+<?php
+    echo "Hello PHP!! <br>";
+    echo 'Hello PHP!! <br>';
+    echo "Hello " . "PHP" . "!! <br> <br>";
+?>
